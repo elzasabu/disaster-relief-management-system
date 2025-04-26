f368c3f22bdecbcbd9e54de9ad33d2e282b08641
elza-sabu <elzasabu794@gmail.com>
2025-04-26 12:23:26 +0530

    1st commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+app.listen(3000, () => console.log("Server started on port 3000"));
